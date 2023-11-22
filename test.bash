08fc34b159b85449725e496a756274856443dc44
Qiao Qiao <qiaoq@uchicago.edu>
2023-11-22 10:03:06 -0600

    remove old tests

--- test.bash
+++ test.bash
@@ -4,11 +4,10 @@ trap "echo -e '\x1b[01;31mFailed\x1b[0m'" ERR
 
 python -m yolk --query-metadata=pip | grep -i 'license:'
 python -m yolk --query-metadata=pip --fields=author,name | grep -i 'author:'
-! python -m yolk --query-metadata=pip --fields=author,name | grep -i 'license:'
+python -m yolk --query-metadata=pip --fields=author,name | grep -i 'license:' && exit 1
 
 # Do not print field name if there is only one.
-! python -m yolk --query-metadata=pip --fields=author | grep -i 'author:'
-
+python -m yolk --query-metadata=pip --fields=author | grep -i 'author:' && exit 1
 python -m yolk --depends=pip
 python -m yolk --list
 python -m yolk --list pip
@@ -18,9 +17,11 @@ python -m yolk --list --metadata --fields=author,license | grep -i 'license:'
 
 python -m yolk --depends=fake_foo 2>&1 | grep 'fake_foo is not installed'
 
-python -m yolk --latest-releases=1
-python -m yolk --show-updates
-python -m yolk --show-updates --user
+# not supported
+#python -m yolk --latest-releases=1
+# overwhelming server
+#python -m yolk --show-updates
+#python -m yolk --show-updates --user
 
 python -m doctest yolk/utils.py
 
